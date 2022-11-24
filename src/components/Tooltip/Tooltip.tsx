5046a4d7842b26b4e6ad9d8631ab13e543901898
Morten Brudvik <morten@brudvik.com>
2022-11-24 22:13:04 +0100

    chore: coding along

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,10 +1,26 @@
-export type TooltipProps = {
-    text: string,
+import {ReactNode, useState} from "react";
+import {offset, useFloating} from "@floating-ui/react-dom-interactions";
+
+export type TooltipProps = {    
+    children: ReactNode,   
+    text: string
 }
 
-export const Tooltip = ({text}: TooltipProps) => {
-
+export const Tooltip = ({children}: TooltipProps) => {
+    const [open, setOpen] = useState(false);
+    
+    const {
+        x,y,
+        reference, floating
+    } = useFloating({
+        open,
+        middleware: [
+            offset(8),
+        ],
+        strategy: 'absolute',
+        onOpenChange: setOpen
+    })
     return (
-        <div>{text}</div>
+        <div>{children}</div>
     );
 }
