8029aa326f8ffd2edef1cb36c2ccf4eafd0ae286
Morten Brudvik <morten@brudvik.com>
2022-11-27 18:43:14 +0100

    chore: Add style, more code

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,26 +1,68 @@
-import {ReactNode, useState} from "react";
-import {offset, useFloating} from "@floating-ui/react-dom-interactions";
+import {cloneElement, ReactElement, ReactNode, useState} from "react";
+import {
+    flip,
+    offset,
+    shift,
+    Side,
+    useFloating, useFocus,
+    useHover,
+    useInteractions
+} from "@floating-ui/react-dom-interactions";
+import './Tooltip.css';
+
+export type TooltipPlacement = Side;
 
 export type TooltipProps = {    
-    children: ReactNode,   
+    children: ReactElement,   
     text: string
+    placement?: TooltipPlacement
 }
 
-export const Tooltip = ({children}: TooltipProps) => {
+export const Tooltip = ({text, placement = 'top', children}: TooltipProps) => {
     const [open, setOpen] = useState(false);
-    
+    const strategy = 'absolute';
+
     const {
-        x,y,
-        reference, floating
+        x, y,
+        reference, floating,
+        context
     } = useFloating({
         open,
+        placement,
         middleware: [
             offset(8),
+            flip(),
+            shift({padding: 5})
         ],
-        strategy: 'absolute',
+        strategy: strategy,
         onOpenChange: setOpen
-    })
+    });
+    
+    const {getReferenceProps, getFloatingProps} = useInteractions([
+        useHover(context, {
+            delay: {
+                open: 200,
+                close: 0
+            }
+        }),
+        useFocus(context)
+    ])
+    
     return (
-        <div>{children}</div>
+        <div>
+            {cloneElement(children, {ref: reference})}
+            {open && (
+                <div
+                    ref={floating}
+                    style={{
+                        position: strategy,
+                        top: y ?? 0,
+                        left: x ?? 0,
+                    }}
+                >
+                    {text}
+                </div>
+            )}
+        </div>
     );
 }
